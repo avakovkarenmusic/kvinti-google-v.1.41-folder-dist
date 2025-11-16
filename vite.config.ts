87714d0d9e825f52208179058b04139a5a09e179
Karen <avakov.karen.music@gmail.com>
2025-11-16 11:46:30 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/kvinti-google-v.1.41-folder-dist/', // важно! название репозитория
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
